8b06a99156348c5f6ccca8a3a62cb3198ccc0fbd
schnaider30-programmer <sjeanlouis@byupathway.edu>
2026-03-31 16:16:59 -0400

    disable version key (_v)

--- models/courses.js
+++ models/courses.js
@@ -1,12 +1,15 @@
 const { Schema } = require("mongoose");
 
-const courseSchema = new Schema({
-  courseId: { type: String, required: true },
-  title: { type: String, required: true },
-  credits: { type: Number, required: true },
-  instructor: { type: String, required: true },
-  semester: { type: String, require: true },
-});
+const courseSchema = new Schema(
+  {
+    courseId: { type: String, required: true },
+    title: { type: String, required: true },
+    credits: { type: Number, required: true },
+    instructor: { type: String, required: true },
+    semester: { type: String, require: true },
+  },
+  { versionKey: false },
+);
 
 const Course = require("mongoose").model("Course", courseSchema);
 
